ab1daf1d2dddfe383da98771009a69a9bd37e2e3
Doug Sweetser <sweetser@alum.mit.edu>
2020-11-01 22:22:37 -0500

    Added week 3 screen captures.

--- Lecture__screen_captures/label.sh
+++ Lecture__screen_captures/label.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 >&2 echo 'Assumes imagemagick is in stalled. Pipe to the shell.'
-ls 2020*png | perl -lane '$f = $F[0]; $n = $f; $n =~ s/2020.*part_\d*__//; $n =~ s/.png//; print(qq(convert $f -gravity North -pointsize 30 -annotate 0 "$n" $f));'
+ls 2020*png | perl -lane '$f = $F[0]; $n = $f; $n =~ s/2020.*part_\d*__//; $n =~ s/.png//; print(qq(convert $f -gravity North -pointsize 30 -fill yellow -annotate 0 "$n" $f));'
